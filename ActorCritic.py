5965334d78ed414e037a408e6c8bb40882dc60cf
Shi-YiWei <79592783+Shi-YiWei@users.noreply.github.com>
2022-09-16 00:31:48 +0100

    Add files via upload

--- ActorCritic.py
+++ ActorCritic.py
@@ -13,10 +13,8 @@ class ActorCritic:
         self.actor = PolicyNet(state_dim, args.hidden_dim, action_dim).to(device)
         self.critic = ValueNet(state_dim, args.hidden_dim).to(device)  # 价值网络
         # 策略网络优化器
-        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),
-                                                lr=args.actor_lr)
-        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),
-                                                 lr=args.critic_lr)  # 价值网络优化器
+        self.actor_optimizer = torch.optim.RMSprop(self.actor.parameters(), lr=args.actor_lr)
+        self.critic_optimizer = torch.optim.RMSprop(self.critic.parameters(), lr=args.critic_lr)
         self.gamma = args.gamma
         self.device = device
 
@@ -28,29 +26,24 @@ class ActorCritic:
         return action.item()
 
     def update(self, transition_dict):
-        states = torch.tensor(transition_dict['states'],
-                              dtype=torch.float).to(self.device)
-        actions = torch.tensor(transition_dict['actions']).view(-1, 1).to(
-            self.device)
-        rewards = torch.tensor(transition_dict['rewards'],
-                               dtype=torch.float).view(-1, 1).to(self.device)
-        next_states = torch.tensor(transition_dict['next_states'],
-                                   dtype=torch.float).to(self.device)
-        dones = torch.tensor(transition_dict['dones'],
-                             dtype=torch.float).view(-1, 1).to(self.device)
+        states = torch.tensor(transition_dict['states'], dtype=torch.float).to(self.device)
+        actions = torch.tensor(transition_dict['actions']).view(-1, 1).to( self.device)
+        rewards = torch.tensor(transition_dict['rewards'], dtype=torch.float).view(-1, 1).to(self.device)
+        next_states = torch.tensor(transition_dict['next_states'], dtype=torch.float).to(self.device)
+        dones = torch.tensor(transition_dict['dones'], dtype=torch.float).view(-1, 1).to(self.device)
 
         # 时序差分目标
-        td_target = rewards + self.gamma * self.critic(next_states) * (1 -
-                                                                       dones)
+        td_target = rewards + self.gamma * self.critic(next_states) * (1 - dones)
         td_delta = td_target - self.critic(states)  # 时序差分误差
         log_probs = torch.log(self.actor(states).gather(1, actions))
         actor_loss = torch.mean(-log_probs * td_delta.detach())
         # 均方误差损失函数
-        critic_loss = torch.mean(
-            F.mse_loss(self.critic(states), td_target.detach()))
+        critic_loss = torch.mean(F.mse_loss(self.critic(states), td_target.detach()))
         self.actor_optimizer.zero_grad()
         self.critic_optimizer.zero_grad()
         actor_loss.backward()  # 计算策略网络的梯度
         critic_loss.backward()  # 计算价值网络的梯度
         self.actor_optimizer.step()  # 更新策略网络的参数
         self.critic_optimizer.step()  # 更新价值网络的参数
+
+
